45049340555387f311532bd32c1ce49da22376f8
KennyKennethOcitti <kennethocitti95@gmail.com>
2020-02-24 01:13:36 +0300

    Added question to be attached with the answer

--- Challenge1.py
+++ Challenge1.py
@@ -1,7 +1,39 @@
+# def calculatePerimeter(grid, point):
+#    """
+#    Given an MxN grid of integers and a point in the 0-based grid,
+#    calculate the perimeter of the largest island to which that point belongs.
+#    An island is a group of horizontally and vertically contiguous cells with the same value.
+#    Example:
+#    Grid:
+#    ---—*****———-----
+#    | 0 * 1 * 0 | 0 |
+#    —---*—--*———-----
+#    | 2 * 1 * 0 | 1 |
+#    —---*--—*****—---
+#    | 0 * 1 | 1 * 0 |
+#    —---*********—---
+#    | 1 | 0 | 0 | 3 |
+#    ——————-----------
+#    Given point: (1, 1)
+#    The perimeter here is denot/ed in *'s, and would be 10.
+#    @param grid: an MxN array of integers
+#    @param point: (x,y) tuple where x is the coordinate which corresponds to the row and y corresponds to the column
+#    @return: perimeter
+#    """
+#    perimeter = None
+
+#    # Your code goes here
+#    return perimeter	
+  
+
+
+
+
+
 # Python 3 program to calculate perimeter of the largest 
 # island to which the point grid belongs
 
-#An MXN array of intergers
+#An M * N array of intergers
 M = 4
 N = 4
 
